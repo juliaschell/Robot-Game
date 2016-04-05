b2135465d4189bda7fd77ff144fea6ce5645f33a
Julia Schell <juliaschell@Julias-MacBook-Pro.local>
2016-04-05 15:13:06 -0700

    Initial Commit

---
+++ Project 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 1
+//
+//  Created by Julia Schell on 4/5/16.
+//  Copyright (c) 2016 Julia Schell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
