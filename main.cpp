60d51688d3336ffb1dd797739a91005e675ea487
juliaschell <juliaschell@g.ucla.edu>
2018-04-28 16:19:36 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Project 1
+//
+//  Created by Julia Schell on 4/5/16.
+//  Copyright (c) 2016 Julia Schell. All rights reserved.
+//
+
+#include "game.h"
+
+int main()
+{
+    // Create a game
+    // Use this instead to create a mini-game:   Game g(3, 4, 2);
+    Game g(7, 8, 28);
+    
+    // Play the game
+    g.play();
+}
